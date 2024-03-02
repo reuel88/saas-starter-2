970e4466558c51c4c6bc69f791cba127a2f98f33
Reuel Teodoro <12149910+reuel88@users.noreply.github.com>
2024-03-02 19:55:22 +1100

    feat(docs, ui): SS-00003 - add test with docs

---
+++ packages/ui/src/components/button.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { Button } from "./button";
+import { vi } from "vitest";
+
+describe("Button", () => {
+  it("should render button with children", () => {
+    render(<Button appName="Test App">Test Button</Button>);
+
+    const button = screen.getByRole("button");
+
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent("Test Button");
+  });
+
+  it("should render button onClick calls alert", async () => {
+    const spy = vi.spyOn(window, "alert").mockImplementation(() => {});
+    const user = userEvent.setup();
+    render(<Button appName="Test App">Test Button</Button>);
+
+    const button = screen.getByRole("button");
+    await user.click(button);
+
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+});
--- packages/ui/src/components/button.tsx
+++ packages/ui/src/components/button.tsx
@@ -10,11 +10,11 @@ interface ButtonProps {
 
 export const Button = ({ children, className, appName }: ButtonProps) => {
   return (
-    <button
-      className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
-    >
-      {children}
-    </button>
+      <button
+        className={className}
+        onClick={() => alert(`Hello from your ${appName} app!`)}
+      >
+        {children}
+      </button>
   );
 };
